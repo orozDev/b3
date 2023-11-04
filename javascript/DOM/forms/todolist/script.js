270c573007cf01ada919f9e519c2768ec68b779c
Orozbek Zhenishbek uulu <oroz.jenishbek.2003@gmail.com>
2023-11-04 17:09:42 +0600

    04.10.2023

--- javascript/DOM/forms/todolist/script.js
+++ javascript/DOM/forms/todolist/script.js
@@ -1,18 +1,66 @@
 const createTaskForm = document.forms.createTaskForm
 const taskContainer = document.querySelector('#taskContainer')
-const createTaskFormModal = document.getElementById('createTaskModal');
-const modal = bootstrap.Modal.getInstance(createTaskFormModal)
-
+const createTaskFormModal = document.querySelector('#createTaskModal');
+const modal = new bootstrap.Modal(createTaskFormModal)
+const searchInput = document.querySelector('#searchInput');
 
 let taskId = 1
 
+class Task {
+    constructor({name, desc, deadline, isCompleted}) {
+        this.id = taskId
+        this.name = name
+        this.desc = desc
+        this.deadline = deadline
+        this.isCompleted = isCompleted
+        taskId++
+    }
+    
+}
+
+
+const tasks = [
+    new Task({
+        name: 'oroz',
+        desc: 'sadas dasd asd as',
+        deadline: new Date('2023-12-12'),
+        isCompleted: true
+    }),
+    new Task({
+        name: 'nursultan',
+        desc: 'sadas dasd asd as',
+        deadline: new Date('2020-12-12'),
+        isCompleted: true
+    }),
+    new Task({
+        name: 'bahrom',
+        desc: 'sadas dasd asd as',
+        deadline: new Date('2020-12-12'),
+        isCompleted: true
+    }),
+    new Task({
+        name: 'zdsasdas',
+        desc: 'sadas dasd asd as',
+        deadline: new Date('2020-12-12'),
+        isCompleted: true
+    }),
+    new Task({
+        name: 'zdsasdas',
+        desc: 'sadas dasd asd as',
+        deadline: new Date('2020-12-12'),
+        isCompleted: true
+    }),
+]
+
+
 const makeTemplate = ({
-    id, name, desc, isCompleted
+    id, name, desc, deadline, isCompleted
 }) => `
     <tr>
         <th scope="row">${id}</th>
         <td>${name}</td>
         <td><p> ${desc}</p></td>
+        <td><p> ${deadline.toDateString()}</p></td>
         <td><input type="checkbox" ${isCompleted ? 'checked' : ''}" name=""></td>
         <td>
             <div class="d-flex gap-3">
@@ -24,6 +72,12 @@ const makeTemplate = ({
     </tr>
     `
 
+const renderTasks = (listOfTasks) => {
+    taskContainer.innerHTML = ''
+    for (const task of listOfTasks) {
+        taskContainer.innerHTML += makeTemplate(task)
+    }
+}    
 
 createTaskForm.addEventListener('submit', e => {
     e.preventDefault();
@@ -32,33 +86,33 @@ createTaskForm.addEventListener('submit', e => {
     const desc = createTaskForm.desc.value
     const deadline = new Date(createTaskForm.deadline.value)
 
-    taskContainer.innerHTML += makeTemplate({taskId, name, desc, deadline})
-    taskId++
+    tasks.push(new Task({
+        name, desc, deadline: new Date(deadline), isCompleted: false,
+    }))
+
+    console.log(tasks);
+    
+    taskContainer.innerHTML += makeTemplate(tasks.at(-1))
+
     modal.hide();
+    createTaskForm.reset()
     
 })
 
+searchInput.addEventListener('input', e => {
 
-/*
-    let taskId = 1
+    const val = e.target.value
+    const serchedTasks = tasks.filter(task => task.name.includes(val))
+    renderTasks(serchedTasks)
 
-class Task {
-    constructor({
-        name, desc, isCompleted
-    }) {
-        this.id = taskId
-        this.name = name
-        this.desc = desc
-        this.isCompleted = isCompleted
-        taskId++
-    }
+})
+
+const start = () => {
+    taskContainer.innerHTML = `<tr><td colspan="6" align="center"><img src="./loading.gif"></td></tr>`
+    setTimeout(() => {
+          renderTasks(tasks)
+    }, 1000)
+
 }
 
-const tasks = [
-    new Task({
-        name: 'sadas',
-        desc: ''
-    })
-]
-
-*/
+start()
