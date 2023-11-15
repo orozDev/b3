050e239f43fdcdfaa10a7bd6ab238de32b71df17
Orozbek Zhenishbek uulu <oroz.jenishbek.2003@gmail.com>
2023-11-15 14:24:33 +0600

    toast library

--- javascript/DOM/forms/todolist/script.js
+++ javascript/DOM/forms/todolist/script.js
@@ -21,38 +21,42 @@ class Task {
 }
 
 
-const tasks = [
-    new Task({
-        name: 'oroz',
-        desc: 'sadas dasd asd as',
-        deadline: new Date('2023-12-12'),
-        isCompleted: true
-    }),
-    new Task({
-        name: 'nursultan',
-        desc: 'sadas dasd asd as',
-        deadline: new Date('2020-12-12'),
-        isCompleted: true
-    }),
-    new Task({
-        name: 'bahrom',
-        desc: 'sadas dasd asd as',
-        deadline: new Date('2020-12-12'),
-        isCompleted: false
-    }),
-    new Task({
-        name: 'zdsasdas',
-        desc: 'sadas dasd asd as',
-        deadline: new Date('2020-12-12'),
-        isCompleted: false
-    }),
-    new Task({
-        name: 'zdsasdas',
-        desc: 'sadas dasd asd as',
-        deadline: new Date('2020-12-12'),
-        isCompleted: true
-    }),
-]
+const tasks = JSON.parse(localStorage.getItem('tasks') ?? '[]')
+
+console.log(tasks)
+
+//     [
+//     new Task({
+//         name: 'oroz',
+//         desc: 'sadas dasd asd as',
+//         deadline: new Date('2023-12-12'),
+//         isCompleted: true
+//     }),
+//     new Task({
+//         name: 'nursultan',
+//         desc: 'sadas dasd asd as',
+//         deadline: new Date('2020-12-12'),
+//         isCompleted: true
+//     }),
+//     new Task({
+//         name: 'bahrom',
+//         desc: 'sadas dasd asd as',
+//         deadline: new Date('2020-12-12'),
+//         isCompleted: false
+//     }),
+//     new Task({
+//         name: 'zdsasdas',
+//         desc: 'sadas dasd asd as',
+//         deadline: new Date('2020-12-12'),
+//         isCompleted: false
+//     }),
+//     new Task({
+//         name: 'zdsasdas',
+//         desc: 'sadas dasd asd as',
+//         deadline: new Date('2020-12-12'),
+//         isCompleted: true
+//     })
+// ]
 
 
 const makeTemplate = ({
@@ -62,7 +66,7 @@ const makeTemplate = ({
         <th scope="row">${id}</th>
         <td>${name}</td>
         <td><p> ${desc}</p></td>
-        <td><p> ${deadline.toDateString()}</p></td>
+        <td><p> ${new Date(deadline).toLocaleDateString()}</p></td>
         <td><input type="checkbox" onchange="toggleDoneTask(${id})" ${isCompleted ? 'checked' : ''} name=""></td>
         <td>
             <div class="d-flex gap-3">
@@ -74,6 +78,10 @@ const makeTemplate = ({
     </tr>
     `
 
+const reloadData = () => {
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
 const renderTasks = (listOfTasks) => {
     taskContainer.innerHTML = ''
     for (const task of listOfTasks) {
@@ -84,6 +92,7 @@ const renderTasks = (listOfTasks) => {
 const deleteTask = (taskId) => {
     const taskIndex = tasks.findIndex(item => item.id === taskId)
     tasks.splice(taskIndex, 1)
+    reloadData()
     document.querySelector(`#task_${taskId}`).remove()
 
 }
@@ -91,6 +100,7 @@ const deleteTask = (taskId) => {
 const toggleDoneTask = (taskId) => {
     const task = tasks.find(item => item.id === taskId)
     task.isCompleted = !task.isCompleted
+    reloadData()
     console.log(tasks)
 }
 
@@ -102,21 +112,23 @@ const preparingToUpdate = (taskId) => {
     const updateTaskForm = document.forms.updateTaskForm
     updateTaskForm.name.value = task.name
     updateTaskForm.desc.value = task.desc
-    updateTaskForm.deadline.value = task.deadline.toISOString().slice(0, 10)
+    updateTaskForm.deadline.value = task.deadline.slice(0, 10)
     updateTaskForm.setAttribute('data-task_id', taskId)
 
 }
+
 
 createTaskForm.addEventListener('submit', e => {
     e.preventDefault();
 
     const name = createTaskForm.name.value
     const desc = createTaskForm.desc.value
-    const deadline = new Date(createTaskForm.deadline.value)
+    const deadline = new Date(createTaskForm.deadline.value).toISOString()
 
     tasks.push(new Task({
-        name, desc, deadline: new Date(deadline), isCompleted: false,
+        name, desc, deadline, isCompleted: false,
     }))
+    reloadData()
 
     console.log(tasks);
 
@@ -138,7 +150,8 @@ updateTaskForm.addEventListener('submit', e => {
 
     task.name = updateTaskForm.name.value
     task.desc = updateTaskForm.desc.value
-    task.deadline = new Date(updateTaskForm.deadline.value)
+    task.deadline = new Date(updateTaskForm.deadline.value).toISOString()
+    reloadData()
 
     updateTaskModal.hide()
     updateTaskForm.reset()
